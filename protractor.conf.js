c421aa69f21a88af2ff36cc550f79fac82477d93
Aleksei Ogarkov <alexey.ogarkov@gmail.com>
2016-11-01 03:54:38 +0300

    chromeOnly

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,6 +1,7 @@
 require('ts-node/register');
 
 exports.config = {
+  chromeOnly: true,
   directConnect: true,
   baseUrl: 'https://angular.io/',
   specs: ['test/protractor/**/*.spec.ts', 'test/protractor/*.spec.ts']
